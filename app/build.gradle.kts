4e0b3a71e3fd90d49180558e40dfc54dd487809a
scomor55 <scomor2@etf.unsa.ba>
2024-04-26 01:18:16 +0200

    old gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,11 +45,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.test:core-ktx:1.5.0")
-    androidTestImplementation("androidx.test:runner:1.5.2")
-    androidTestImplementation("androidx.test:rules:1.5.0")
-
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
 }
